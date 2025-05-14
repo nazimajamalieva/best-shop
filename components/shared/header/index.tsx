f2d55e758e1df4ed31363815371ba72b76a1de1e
Nazima Jamalieva <nazimazamalieva836@gmail.com>
2025-05-14 15:46:34 +0600

    feat: add theme toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
+
 const Header = () => {
   return (
     <header className="w-full border-b">
@@ -22,6 +24,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart />
