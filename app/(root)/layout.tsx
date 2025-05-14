693fe323076ae7c9738a6c24688141f8c14f42a9
Nazima Jamalieva <nazimazamalieva836@gmail.com>
2025-05-14 15:31:42 +0600

    feat: add header, footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,29 @@
+import type { Metadata } from "next";
+import Footer from "@/components/footer";
+import { Inter } from "next/font/google";
+import Header from "@/components/shared/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "My shop",
+  description: "A modern store built with Next.js",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col">
-      <main className="flex-1 wrapper">{children}</main>
-    </div>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <div className="flex h-screen flex-col">
+          <Header />
+          <main className="flex-1 wrapper">{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
