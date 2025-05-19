f7e7e228d5c26efbcf01a3a633d1cf5c2bed47f4
Nazima Jamalieva <nazimazamalieva836@gmail.com>
2025-05-19 18:20:31 +0600

    feat: add product-images

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -19,7 +20,7 @@ const ProductDetailsPage = async (props: {
         <div className="grid grid-cols-1 md:grid-cols-5 ">
           {/* Images Column */}
           <div className="col-span-2">
-            {/* TODO: Add product images here */}
+            <ProductImages images={product.images!} />
           </div>
 
           {/* Details Column */}
